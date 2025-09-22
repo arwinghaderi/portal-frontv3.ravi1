3272864ee3160cba717cd32bd5a742cb385a094c
arwinGhaderi <arwinghaderi81@gmail.com>
2025-09-22 23:25:33 +0330

    fix(photo-page): finalize route param handling and data fetch logic

--- src/app/(main)/photo/[id]/page.tsx
+++ src/app/(main)/photo/[id]/page.tsx
@@ -5,15 +5,15 @@ import type { TPortfolio } from '@/types'
 export default async function PhotoDetailsPage({
   params,
 }: {
-  params: Promise<{ id: string }>
+  params: { id: string }
 }) {
-  const { id } = await params
+  const { id } = params
   const res = await FGetPortfolioItem({ id, categoryId: 23 })
   const data: TPortfolio = res?.data
 
   if (!data) {
     return (
-      <div className="min-h-[600px] flex items-center justify-center  animate-bounce  text-primary">
+      <div className="min-h-[600px] flex items-center justify-center animate-bounce text-primary">
         موردی یافت نشد.
       </div>
     )
