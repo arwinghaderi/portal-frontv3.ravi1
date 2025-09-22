0c7e9da678e293cfd79571e2aedf0ae31264d086
arwinGhaderi <arwinghaderi81@gmail.com>
2025-09-22 22:59:11 +0330

    fix(app-router): correct invalid Promise usage in dynamic route params

--- src/app/(main)/gallery/[id]/page.tsx
+++ src/app/(main)/gallery/[id]/page.tsx
@@ -1,21 +1,20 @@
-// src/app/(main)/video/[id]/page.tsx
 import { FGetPortfolioItem } from '@/api/api'
 import VideoDetailsView from '@/app/(main)/_components/template/video/VideoDetailsPage'
+
 import type { TPortfolio } from '@/types'
 
-export default async function VideoDetailsPage({
+export default async function GalleryPage({
   params,
 }: {
   params: { id: string }
 }) {
-  const { id } = params
-  const res = await FGetPortfolioItem({ id, categoryId: 23 })
+  const res = await FGetPortfolioItem({ id: params.id, categoryId: 23 })
   const data: TPortfolio = res?.data
 
   if (!data) {
     return (
       <div className="min-h-[600px] flex items-center justify-center text-primary animate-bounce">
-        ویدیو یافت نشد.
+        گالری یافت نشد.
       </div>
     )
   }
